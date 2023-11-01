55c06a750f255b9480e1ad1bcfba0ad310d2aeb6
davidgit3000 <davidkarik5@gmail.com>
2023-11-01 15:52:57 -0700

    Update header

--- frontend/src/Components/SearchBar.js
+++ frontend/src/Components/SearchBar.js
@@ -1,8 +1,14 @@
 import { Card, Stack, Form } from "react-bootstrap";
 import { MDBCol } from "mdbreact";
 import { Search } from "react-bootstrap-icons";
+import { useState } from "react";
 
 export default function SearchBar() {
+  const [input, setInput] = useState("");
+  const fetchArticles = (value) => {
+    
+  }
+
   return (
     <>
       <MDBCol md="10">
@@ -11,6 +17,11 @@ export default function SearchBar() {
             <Form.Control
               className="rounded-pill border-0"
               placeholder="Search articles here..."
+              style={{ "box-shadow": "none" }}
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value)
+              }}
             />
             <Search className="mx-1"></Search>
           </Stack>
