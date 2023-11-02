bc76050d74f0fe93a642a4b9449e42448f5318a2
dannypeeps <113870485+dannypeeps@users.noreply.github.com>
2023-11-02 14:58:09 -0700

    added descrp text box with html and css components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import { Button, Alert, Breadcrumb, Card } from "react-bootstrap";
-
+import React from 'react';
 
 function App() {
   return (
@@ -19,6 +19,29 @@ function App() {
         </Breadcrumb>
         <Alert variant="primary">This is a button</Alert>
         <Button>Text button</Button>
+
+        {/* Description Textbox */}
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: '20px', // makes some space away from other components, may change 
+        }}>
+            <div style={{
+              backgroundColor: '#CECECE', // box background color
+              color: 'black',             // font color
+              padding: '20px',            
+              borderRadius: '10px',       // roundness corners of box
+              width: '80%',               // 80% of parent container
+              textAlign: 'center',
+            }}>
+                <p> 
+                  lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                  Vestibulum non elementum erat. Duis id metus semper, pharetra 
+                  ipsum quis, commodo ligula.
+                </p>
+              </div>
+        </div>
     </div>
   );
 }
