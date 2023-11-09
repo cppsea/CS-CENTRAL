7f5be33a925fb3d503d6cc48a8994cee0c448eb8
Patrick Nguyen <patricknguy789@gmail.com>
2023-11-09 14:00:07 -0800

    change title query to title that contains string

--- server/src/articles/queries.js
+++ server/src/articles/queries.js
@@ -1,7 +1,7 @@
 const getArticles = "SELECT * FROM articles";
 const getArticlesById = "SELECT * FROM articles WHERE id = $1";
 const addArticles = "INSERT INTO articles (title) VALUES ($1)";
-const getArticlesByTitle = "SELECT * FROM articles WHERE title = $1";
+const getArticlesByTitle = "SELECT * FROM articles WHERE title LIKE '%' || $1 || '%'";
 
 module.exports = {
   getArticles,
