92747f096dfd75d07847efe80fda66f7a7b940e6
davidgit3000 <davidkarik5@gmail.com>
2024-01-01 11:42:44 -0800

    modify client authentiation (not using Alerts component) - provide feedback instead - add show/hide password - add email validation using Regex

--- frontend/src/Components/Signup/SignupCard.js
+++ frontend/src/Components/Signup/SignupCard.js
@@ -1,7 +1,9 @@
-import { useState } from "react";
-import { Card, Form, Button, Alert } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Card, Form, Button, InputGroup } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import * as auth from "../auth/auth";
+
+import { EyeFill, EyeSlashFill } from "react-bootstrap-icons";
 
 export default function SignupCard() {
   const [formVal, setFormVal] = useState({
@@ -12,9 +14,15 @@ export default function SignupCard() {
     password: "",
     confirmPassword: "",
   });
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const navigate = useNavigate();
+
+  //whether form has run through validation yet
+  const [isValidated, setValidated] = useState(false);
 
   // error messages
-  const [errorMessages, setErrorMessages] = useState([]);
+  const [errorMessages, setErrorMessages] = useState({});
 
   // handle input entered
   const handleInput = (e) => {
@@ -26,11 +34,30 @@ export default function SignupCard() {
     });
   };
 
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleConfirmPasswordToggle = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  const isValidationPassed = () => {
+    return Object.keys(errorMessages).length === 0 ? true : false;
+  };
+
+  useEffect(() => {
+    // might add API endpoints to handle backend authentication here
+    if (isValidated && isValidationPassed()) {
+      navigate("/signin");
+    }
+  }, [isValidationPassed]);
+
   // handle submit
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const errMessagesList = [];
+    const newErrMessages = {};
     const formValidation = auth.formValidation;
 
     for (const fieldName in formValidation) {
@@ -44,12 +71,13 @@ export default function SignupCard() {
         );
 
         if (typeof validateResult === "string") {
-          errMessagesList.push(validateResult);
+          newErrMessages[fieldName] = validateResult;
         }
       });
     }
 
-    setErrorMessages(errMessagesList);
+    setValidated(true);
+    setErrorMessages(newErrMessages);
   };
 
   return (
@@ -57,26 +85,24 @@ export default function SignupCard() {
       <Card className="rounded-3 w-signup">
         <Card.Body className="bg-signin-card rounded-3">
           <Card.Title className="text-center fs-2 fw-bold">Sign up</Card.Title>
-          {errorMessages.map((err) => (
-            <Alert
-              key={err}
-              dismissible
-              variant="warning"
-              className="my-auto mt-4"
-            >
-              &#9888; {err}
-            </Alert>
-          ))}
 
-          <Form>
+          <Form noValidate validated={isValidated} onSubmit={handleSubmit}>
             <div className="d-flex justify-content-between gap-2">
-              <Form.Group className="my-4">
+              <Form.Group className="my-4 is-invalid">
                 <Form.Control
                   name="fname"
                   value={formVal.fname}
                   placeholder="First Name"
                   onChange={handleInput}
+                  isInvalid={errorMessages.hasOwnProperty("fname")}
+                  required
                 />
+
+                <Form.Control.Feedback type="invalid">
+                  {errorMessages.hasOwnProperty("fname")
+                    ? errorMessages.fname
+                    : ""}
+                </Form.Control.Feedback>
               </Form.Group>
               <Form.Group className="my-4">
                 <Form.Control
@@ -84,7 +110,14 @@ export default function SignupCard() {
                   value={formVal.lname}
                   placeholder="Last Name"
                   onChange={handleInput}
+                  isInvalid={errorMessages.hasOwnProperty("lname")}
+                  required
                 />
+                <Form.Control.Feedback type="invalid">
+                  {errorMessages.hasOwnProperty("lname")
+                    ? errorMessages.lname
+                    : ""}
+                </Form.Control.Feedback>
               </Form.Group>
             </div>
             <Form.Group>
@@ -93,7 +126,14 @@ export default function SignupCard() {
                 value={formVal.username}
                 placeholder="Username"
                 onChange={handleInput}
+                isInvalid={errorMessages.hasOwnProperty("username")}
+                required
               />
+              <Form.Control.Feedback type="invalid">
+                {errorMessages.hasOwnProperty("username")
+                  ? errorMessages.username
+                  : ""}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="my-4">
               <Form.Control
@@ -102,28 +142,67 @@ export default function SignupCard() {
                 value={formVal.email}
                 placeholder="Email"
                 onChange={handleInput}
+                isInvalid={errorMessages.hasOwnProperty("email")}
+                required
               />
+              <Form.Control.Feedback type="invalid">
+                {errorMessages.hasOwnProperty("email")
+                  ? errorMessages.email
+                  : ""}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group className="my-4">
-              <Form.Control
-                name="password"
-                value={formVal.password}
-                type="password"
-                placeholder="Password"
-                onChange={handleInput}
-              />
+              <InputGroup hasValidation>
+                <Form.Control
+                  name="password"
+                  value={formVal.password}
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  onChange={handleInput}
+                  isInvalid={errorMessages.hasOwnProperty("password")}
+                  required
+                />
+                <Button
+                  title={showPassword ? "hide password" : "show password"}
+                  className="my-auto bg-white border-white"
+                  onClick={handlePasswordToggle}
+                >
+                  {showPassword ? <EyeSlashFill /> : <EyeFill />}
+                </Button>
+                <Form.Control.Feedback type="invalid">
+                  {errorMessages.hasOwnProperty("password")
+                    ? errorMessages.password
+                    : ""}
+                </Form.Control.Feedback>
+              </InputGroup>
             </Form.Group>
             <Form.Group className="my-4">
-              <Form.Control
-                name="confirmPassword"
-                value={formVal.confirmPassword}
-                type="password"
-                placeholder="Confirm Password"
-                onChange={handleInput}
-              />
+              <InputGroup hasValidation>
+                <Form.Control
+                  name="confirmPassword"
+                  value={formVal.confirmPassword}
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="Confirm Password"
+                  onChange={handleInput}
+                  isInvalid={errorMessages.hasOwnProperty("confirmPassword")}
+                  required
+                />
+                <Button
+                  title={showConfirmPassword ? "hide password" : "show password"}
+                  className="my-auto bg-white border-white"
+                  onClick={handleConfirmPasswordToggle}
+                >
+                  {showConfirmPassword ? <EyeSlashFill /> : <EyeFill />}
+                </Button>
+                <Form.Control.Feedback type="invalid">
+                  {errorMessages.hasOwnProperty("confirmPassword")
+                    ? errorMessages.confirmPassword
+                    : ""}
+                </Form.Control.Feedback>
+              </InputGroup>
             </Form.Group>
             <div className="d-grid">
-              <Button onClick={handleSubmit} className="fw-bold text-white">
+              <Button type="submit" className="fw-bold text-white">
                 Register
               </Button>
             </div>
--- frontend/src/Components/auth/auth.js
+++ frontend/src/Components/auth/auth.js
@@ -5,23 +5,37 @@ const errorLabels = {
   email: "email",
   fname: "first name",
   lname: "last name",
-  confirmPassword: "confirmed password",
+  confirmPassword: "confirm password",
+};
+
+// Regex patterns
+const patterns = {
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\d\w@-]{8,20}$/i,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{3,8})$/,
+  //      email_address @  domain    .    com
 };
 
 // functions for validating input fields (add more if possible)
 export const validationFunctions = {
   checkEmpty: (name = "", value1 = "", value2 = "") =>
-    value1.length > 0 || `The ${errorLabels[name]} field should not be empty`,
+    value1.length > 0 || `${errorLabels[name]} is required`,
 
   checkPasswordLength: (name = "", value1 = "", value2 = "") =>
     value1.length >= 8 ||
     value1.length === 0 ||
-    `The password should be no less than 8 characters in length`,
+    "The password should be no less than 8 characters in length",
 
   checkPasswordMatch: (name = "", value1 = "", value2 = "") =>
     value1 === value2 ||
     value1.length === 0 ||
-    `The confirmed password does not match the password above`,
+    "The confirm password does not match the password above",
+
+  checkValidEmail: (name = "", value1 = "", value2 = "") => {
+    const regex = patterns[name];
+
+    return regex.test(value1) || value1.length === 0 ? true : "Invalid email";
+  },
 };
 
 // an object containing input fields (keys)
@@ -30,7 +44,7 @@ export const formValidation = {
   username: [validationFunctions.checkEmpty],
   fname: [validationFunctions.checkEmpty],
   lname: [validationFunctions.checkEmpty],
-  email: [validationFunctions.checkEmpty],
+  email: [validationFunctions.checkEmpty, validationFunctions.checkValidEmail],
   password: [
     validationFunctions.checkEmpty,
     validationFunctions.checkPasswordLength,
