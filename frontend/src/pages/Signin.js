6737ca4542a08803f14fefd6989984fff68b0eed
Darren <98421648+Darren52BT@users.noreply.github.com>
2024-01-06 16:11:20 -0800

    #!9: Constructed Signin/Signup Pages
    * created logo component and signin/signup pages
    
    * refactored logo to use more bootstrap classes

---
+++ frontend/src/pages/Signin.js
@@ -0,0 +1,27 @@
+import { Col, Container, Row, Stack } from "react-bootstrap";
+import SigninCard from "../Components/Signin/SigninCard";
+import Header from "../Components/Header";
+import Logo from "../Components/Signin_Signup/Logo";
+export default function Signin() {
+  return (
+    <>
+      <Header />
+      <Stack className="sign-page-background py-5 d-flex align-items-center justify-content-center">
+        <Container
+          fluid
+          className="d-flex align-items-center"
+          style={{ maxWidth: "1000px" }}
+        >
+          <Row className="w-100 gy-5 m-0">
+            <Col xs={12} sm={6}>
+              <Logo />
+            </Col>
+            <Col xs={12} sm={6}>
+              <SigninCard />
+            </Col>
+          </Row>
+        </Container>
+      </Stack>
+    </>
+  );
+}
