da6fae764e3b91cda2e54ce43f44bd97301529e4
davidgit3000 <davidkarik5@gmail.com>
2024-01-10 17:40:46 -0800

    added Footer, changed the route for Signin and Signup in App.jsx, and change the view height of Signed Pages

--- frontend/src/pages/BasePage.jsx
+++ frontend/src/pages/BasePage.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Components/Header";
-import { Container, Stack } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import Footer from "../Components/Footer/Footer";
+
 export default function BasePage() {
   return (
     <Container fluid className="p-0 min-vh-100">
       <Header />
       <Outlet />
+      <Footer />
     </Container>
   );
 }
