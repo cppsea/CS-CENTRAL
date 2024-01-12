580c389086126654b9f0d57f880fe74b2bbb31a9
ryagatep <135666004+ryagatep@users.noreply.github.com>
2024-01-12 00:52:26 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const articleRoutes = require("./src/articles/routes.js");
+const courseRoutes = require("./src/courses/routes.js");
 const userRoutes = require("./src/users/routes.js");
 const cors = require("cors");
+
+
 
 
 const app = express();
@@ -14,6 +17,8 @@ app.get("/", (req, res) => {});
 
 //routes to /api/articles will be handled in /src/articles/routes.js
 app.use("/api/articles", articleRoutes);
+app.use("/api/courses", courseRoutes);
 app.use('/api/users', userRoutes);
+
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
