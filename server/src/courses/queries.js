308e0468c4fbba6471eca3be0f439033a2fb60ef
ryagatep <135666004+ryagatep@users.noreply.github.com>
2024-01-12 01:38:54 -0800

    Update queries.js

--- server/src/courses/queries.js
+++ server/src/courses/queries.js
@@ -1,7 +1,7 @@
 const deleteCourses = "DELETE FROM courses WHERE id = $1";
 const getCourses = "SELECT * FROM courses";
-const postCourses = "UPDATE courses SET name = $1, description = $2 WHERE id = $3";
-const putCourses = "INSERT INTO courses (name, description) VALUES ($1, $2)";
+const putCourses = "UPDATE courses SET name = $1, description = $2 WHERE id = $3";
+const postCourses = "INSERT INTO courses (name, description) VALUES ($1, $2)";
 
 
 module.exports = {
