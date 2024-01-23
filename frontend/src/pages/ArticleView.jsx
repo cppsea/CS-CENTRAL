f3e67b2ddddeaab6633620f793960bc4e813c63f
CerealAaa <86446120+CerealAaa@users.noreply.github.com>
2024-01-22 17:44:22 -0800

    quick fix to not returning right page

--- frontend/src/pages/ArticleView.jsx
+++ frontend/src/pages/ArticleView.jsx
@@ -7,10 +7,11 @@ export default function ArticleView() {
   const { name = "" } = useParams();
 
   useEffect(() => {
-    fetch(`http://localhost:3002/api/articles/?name=${name}`)
-      .then((res) =>
-        res.json().then((data) => {
+    fetch(`http://localhost:3002/api/articles/${name}`)
+      .then((res) => res.json()
+      .then((data) => {
           let article = data[0];
+          console.log(data);
           setArticle(article);
         })
       )
@@ -18,8 +19,6 @@ export default function ArticleView() {
         console.error("error fetching data");
       });
   }, [name]);
-
-  console.log("ID: ", name);
   return (
     <>
       {/*conditional rendering based on data being fetched*/}
