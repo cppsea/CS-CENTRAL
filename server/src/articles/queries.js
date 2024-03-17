0a2c7c62027694cebbc3cb7f76daf6b73cdeee6d
ryagatep <135666004+ryagatep@users.noreply.github.com>
2024-03-16 17:06:50 -0700

    Update queries.js with getRecommendedArticles

--- server/src/articles/queries.js
+++ server/src/articles/queries.js
@@ -1,13 +1,16 @@
 const getArticles = "SELECT * FROM articles";
 const getArticlesById = "SELECT * FROM articles WHERE id = $1";
-const addArticles = "INSERT INTO articles (title,author_id,headers) VALUES ($1,$2,$3)";
+const getRecommendedArticles = "SELECT * FROM articles ORDER BY num_views DESC LIMIT $1";
+const addArticles = "INSERT INTO articles (title) VALUES ($1)";
 const getArticlesByTitle = "SELECT * FROM articles WHERE title = $1";
 const deleteArticle = "DELETE FROM articles WHERE id = $1";
 const replaceArticle = "";
 
+
 module.exports = {
   getArticles,
   getArticlesById,
+  getRecommendedArticles,
   addArticles,
   getArticlesByTitle,
   deleteArticle,
