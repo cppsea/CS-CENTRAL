423a9e9bbe75565f01245b03b678096a174b0a69
ryagatep <135666004+ryagatep@users.noreply.github.com>
2024-03-16 17:07:39 -0700

    Update routes.js with /recommendations

--- server/src/articles/routes.js
+++ server/src/articles/routes.js
@@ -11,6 +11,7 @@ router.use(requireAuth)
 
 router.get("/", async (req, res) => controller.getArticles(req, res));
 router.get("/:id", (req, res) => controller.getArticlesById(req, res));
+router.get("/recommendations", async (req, res) => controller.getRecommendedArticles(req, res));
 
 router.post("/", (req, res) => controller.addArticles(req, res));
 router.delete('/:id', requireAuth, authorizeArticle, controller.deleteArticle);
