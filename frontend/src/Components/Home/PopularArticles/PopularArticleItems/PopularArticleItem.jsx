b74f1044973d0bcebfe4b7f07264c15c00332ae8
davidgit3000 <davidkarik5@gmail.com>
2024-03-22 15:14:16 -0700

    fixed the title width and image width for popular articles section

--- frontend/src/Components/Home/PopularArticles/PopularArticleItems/PopularArticleItem.jsx
+++ frontend/src/Components/Home/PopularArticles/PopularArticleItems/PopularArticleItem.jsx
@@ -5,14 +5,10 @@ import ArticleImage from "../ArticleImage/ArticleImage";
 export default function PopularArticleItem({ title, article_img, number }) {
   return (
     <>
-      <Stack className="d-flex gap-3 p-3 popular-article-item-container">
-        <div>
-          <ArticleImage article_img={article_img} number={number} />
-        </div>
-        <div>
-          <a className="fw-medium popular-article-title">{title}</a>
-        </div>
-      </Stack>
+      <Container className="d-flex flex-column gap-3 p-3 popular-article-item-container">
+        <ArticleImage article_img={article_img} number={number} />
+        <a className="fw-medium popular-article-title">{title}</a>
+      </Container>
     </>
   );
 }
