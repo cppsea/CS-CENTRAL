13fdea1068481cb82d16a494e8eaaae2a32890c6
ryagatep <135666004+ryagatep@users.noreply.github.com>
2024-04-16 14:34:34 -0700

    Update queries.js

--- server/src/articles/queries.js
+++ server/src/articles/queries.js
@@ -1,8 +1,9 @@
 const getArticles = "SELECT * FROM articles";
 const getArticlesById = "SELECT * FROM articles WHERE id = $1";
-const getRecommendedArticles = "SELECT * FROM articles ORDER BY num_views DESC LIMIT $1";
+const getRecommendedArticles = "SELECT * FROM articles WHERE topic IN (SELECT topic FROM user_preferences WHERE user_id = $2) ORDER BY num_views DESC LIMIT $1";
 const addArticles = "INSERT INTO articles (title) VALUES ($1)";
 const getArticlesByTitle = "SELECT * FROM articles WHERE title = $1";
+const trackUserInteraction = "INSERT INTO user_interactions (user_id, article_id, action_type) VALUES ($1, $2, $3)";
 const deleteArticle = "DELETE FROM articles WHERE id = $1";
 const replaceArticle = "";
 
@@ -13,6 +14,7 @@ module.exports = {
   getRecommendedArticles,
   addArticles,
   getArticlesByTitle,
+  trackUserInteraction,
   deleteArticle,
   replaceArticle,
 };
