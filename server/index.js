1c4164eee76d82d50e2396bc700866b43055f9c6
CerealAaa <86446120+CerealAaa@users.noreply.github.com>
2024-05-30 03:40:28 -0700

    change so only check authorization for creating/deleting articles and not for getting

--- server/index.js
+++ server/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
+const create_delete_articleRoutes = require("./src/articles/cdroutes.js");
 const articleRoutes = require("./src/articles/routes.js");
 const courseRoutes = require("./src/courses/routes.js");
 const userRoutes = require("./src/users/routes.js");
 const cors = require("cors");
 require("dotenv").config();
 
-
 console.log(process.env.PGUSER);
-
-
-
 
 const app = express();
 
@@ -22,7 +19,10 @@ app.use(express.json());
 app.get("/", (req, res) => {});
 
 //routes to /api/articles will be handled in /src/articles/routes.js
-app.use("/api/articles", articleRoutes);
+
+app.use("/api/articles", articleRoutes);// must go BEFORE create_delete because create_delete will return authorization error before allowing the api route for GETting articles
+app.use("/api/articles", create_delete_articleRoutes);//the create_delete will have authorization checked, but the regular articleRoutes will not
+
 app.use("/api/courses", courseRoutes);
 app.use('/api/users', userRoutes);
 
--- server/src/articles/routes.js
+++ server/src/articles/routes.js
@@ -1,18 +1,13 @@
 const { Router } = require("express");
 const controller = require("./controller");
 
-const requireAuth = require('../middleware/requireAuth');
 const authorizeArticle = require('../middleware/authorizeArticle')
 const router = Router();
 
 
 //require auth for all article routes
-router.use(requireAuth)
 
 router.get("/", async (req, res) => controller.getArticles(req, res));
 router.get("/:id", (req, res) => controller.getArticlesById(req, res));
-
-router.post("/", (req, res) => controller.addArticles(req, res));
-router.delete('/:id', (req,res) => controller.deleteArticle(req,res));
 
 module.exports = router;
