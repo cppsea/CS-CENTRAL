02b7ecc27eb5ac87d9b437f69bccc60f9bf3cc78
plexin123 <67119125+plexin123@users.noreply.github.com>
2024-06-19 14:29:34 -0700

    login/signup 90%
    fix some issues

--- frontend/src/hooks/useLogin.jsx
+++ frontend/src/hooks/useLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
+import { Navigate, useNavigate } from "react-router-dom";
 
 
 export const useLogin = () =>
@@ -7,14 +8,16 @@ export const useLogin = () =>
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(false)
     const { dispatch } = useAuthContext()
+    const navigate = useNavigate()
     const apiUrl = import.meta.env.VITE_API_URL; //change to .env
     // const api = process.env.REACT_APP_API_URL
     
     
-        const login = async (username,user_password) => 
+        const login = async (username,user_password,error) => 
     {
         setIsLoading(true);
         setError(null);
+        try {
         const response = await fetch(`${apiUrl}/api/users/login`,
         {
             method : 'POST',
@@ -39,8 +42,16 @@ export const useLogin = () =>
             dispatch({type: 'LOGIN', payload: json})
 
             setIsLoading(false)
+            console.log(localStorage)
+            navigate("/")
         }
+    
     }
+    catch (err) {
+        setError("Something went wrong. Please try again.");
+        setIsLoading(false);
+    }
+}
 
     return {login, isLoading, error}
 }
--- frontend/src/hooks/useSignup.jsx
+++ frontend/src/hooks/useSignup.jsx
@@ -19,7 +19,7 @@ export const useSignup = () => {
         setError(null);
 
         try {
-            const response = await fetch(`http://localhost:3002/api/users`, {
+            const response = await fetch(`${apiUrl}/api/users`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, user_password })
