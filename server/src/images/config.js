e024ec423ef4d4e1dc24bbb0eedbf690fcd48f85
CerealAaa <86446120+CerealAaa@users.noreply.github.com>
2024-07-30 18:37:48 -0700

    changed config.js
    moving keys to .env, paul will change the api keys on cloudinary

--- server/src/images/config.js
+++ server/src/images/config.js
@@ -1,9 +1,10 @@
 const cloudinary = require('cloudinary').v2;
+require("dotenv").config();
 // export const cloudinary1 = cloudinary
 cloudinary.config({
-    cloud_name: 'dfqmccik2',
-    api_key: '274176942952433',
-    api_secret: 'xTN2VnnA_ePTXg9eYlaFu-SNQtw'
+    cloud_name: process.env.CLOUDINARY_CLOUDNAME,
+    api_key: process.env.CLOUDINARY_APIKEY,
+    api_secret: process.env.CLOUDINARY_APISECRET
 });
 
 module.exports = {
